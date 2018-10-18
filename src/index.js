e4b1f01a29ea4e23fc3ad981883eb7884de9cecb
hellomac87 <hellomac87@gmail.com>
2018-10-18 18:34:53 +0900

    ohonghong

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const gameState = {
 
 // *** 화면 그리는 함수
 function drawInning() {
-
-  console.log('drawInning');
   const myInning = `
   <li>
-    <input type="text" value="${gameState.userInput[0]}" disabled/>
-    <input type="text" value="${gameState.userInput[1]}" disabled/>
-    <input type="text" value="${gameState.userInput[2]}" disabled/>
+    <div class="wrap">
+      <input type="text" value="${gameState.userInput[0]}" disabled/>
+      <input type="text" value="${gameState.userInput[1]}" disabled/>
+      <input type="text" value="${gameState.userInput[2]}" disabled/>
+    </div>
     <div class="result">
       ${gameState.ball}ball, ${gameState.strike}strike
     </div>
@@ -29,7 +29,7 @@ function init() {
   // 문제 배열 생성 : question 배열에 0 ~ 9 까지 랜덤 생성된 중복없는 숫자를 생성하여 추가한다.
   gameState.question = [];
   for (let i = 0; i < 3; i++) {
-    const number = Math.floor(Math.random() * 9).toString();
+    const number = Math.floor(Math.random() * 9);
     if (gameState.question.includes(number)) {
       i--;
     } else {
@@ -39,6 +39,9 @@ function init() {
 
   // 유저 입력 배열 초기화하기 : 유저 입력 배열을 초기화한다.(빈 상태로 만든다.)
   gameState.userInput = [];
+
+  document.querySelector('.inning').innerHTML = null;
+  document.querySelector('.input-form').reset();
 
   // TODO : dom 확인용 나중에 지우자 : 테스트를 위해 html 에 뿌려준다.
   document.getElementById('question').innerHTML = `${gameState.question}`
@@ -66,12 +69,24 @@ document.querySelector('.input-form').addEventListener('submit', (e) => {
   e.preventDefault();
 
   elements.inputUsers.forEach(item => {
-    gameState.userInput.push(item.value); // 사용자 입력 상태 업데이트
+    // 사용자 입력 상태 업데이트 : 입력되는 문자열을 숫자로 변환 한뒤 userInput 배열에 넣기
+    gameState.userInput.push(parseInt(item.value));
   });
   // 로직 확인하기
   isStrike();
+  //
+
   // 현재 상태로 화면 그리기
   drawInning();
+
+  if (gameState.strike === 3) {
+    alert('삼진');
+    init();
+  }
+  if (document.querySelectorAll('.inning li').length === 9) {
+    alert('end')
+  }
+  console.log()
   // 화면 그린 후 유저배열 지우기
   gameState.userInput = [];
   gameState.strike = 0;
@@ -95,7 +110,6 @@ function isStrike() {
       }
     }
   }
-
   console.log(`${gameState.strike} strike`);
   console.log(`${gameState.ball} ball`);
 
