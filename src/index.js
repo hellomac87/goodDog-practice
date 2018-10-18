4879d349d654d5255f5ba631ee64c867e6d3d952
hellomac87 <hellomac87@gmail.com>
2018-10-18 10:33:24 +0900

    html 추가하기

--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ const gameState = {
   strike: 0,
   ball: 0,
 }
+
+// *** 화면 그리는 함수
+function drawInning() {
+
+  console.log('drawInning');
+  const myInning = `
+  <li>
+    <input type="text" value="${gameState.userInput[0]}" disabled/>
+    <input type="text" value="${gameState.userInput[1]}" disabled/>
+    <input type="text" value="${gameState.userInput[2]}" disabled/>
+    <div class="result">
+      ${gameState.ball}ball, ${gameState.strike}strike
+    </div>
+  </li>
+  `
+  document.querySelector('.inning').insertAdjacentHTML('beforeend', myInning);
+}
+// *** 상태를 핸들링하는 함수
+
 // 초기화 함수
 function init() {
-  // 문제 배열 만들기
+
+  // 문제 배열 생성 : question 배열에 0 ~ 9 까지 랜덤 생성된 중복없는 숫자를 생성하여 추가한다.
   gameState.question = [];
   for (let i = 0; i < 3; i++) {
     const number = Math.floor(Math.random() * 9).toString();
@@ -16,27 +36,25 @@ function init() {
       gameState.question.push(number);
     }
   }
-  // 유저 배열 초기화하기
+
+  // 유저 입력 배열 초기화하기 : 유저 입력 배열을 초기화한다.(빈 상태로 만든다.)
   gameState.userInput = [];
-  // TODO : dom 확인용 나중에 지우자
+
+  // TODO : dom 확인용 나중에 지우자 : 테스트를 위해 html 에 뿌려준다.
   document.getElementById('question').innerHTML = `${gameState.question}`
 }
 
-// *** 화면 그리는 함수
-function drawInning() {
-  console.log('drawInning');
-}
-// *** 상태를 핸들링하는 함수
-
 // reset 버튼 눌렀을 때
 document.getElementById('btnReset').addEventListener('click', (e) => {
-  e.preventDefault();
-  document.querySelector('.input-form').reset();
+  e.preventDefault(); // 기존의 submit 이벤트를 막는다.
+  document.querySelector('.input-form').reset(); // form 엘리먼트 내, 요소들의 입력값을 초기화한다.
 });
 
 // 유저 입력 이벤트
 document.querySelectorAll('.input-users').forEach((inputUser, index) => {
+  // 각 유저 입력 input 요소에 이벤트를 input 이벤트를 추가한다.
   inputUser.addEventListener('input', (e) => {
+    // input 요소에 입력이 발생하면 다음 input 엘리먼트로 포커스한다.
     inputUser.nextElementSibling.focus();
   });
 });
@@ -51,18 +69,22 @@ document.querySelector('.input-form').addEventListener('submit', (e) => {
     gameState.userInput.push(item.value); // 사용자 입력 상태 업데이트
   });
   // 로직 확인하기
-  logic();
-  // 확인 후 유저배열 지우기
-  gameState.userInput = [];
+  isStrike();
   // 현재 상태로 화면 그리기
   drawInning();
+  // 화면 그린 후 유저배열 지우기
+  gameState.userInput = [];
+  gameState.strike = 0;
+  gameState.ball = 0;
+
 });
 
 // *** logic
 
-function logic() {
+function isStrike() {
   console.log(gameState.question);
   console.log(gameState.userInput);
+
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       if (i === j && gameState.question[i] === gameState.userInput[j]) {
@@ -76,13 +98,8 @@ function logic() {
 
   console.log(`${gameState.strike} strike`);
   console.log(`${gameState.ball} ball`);
-  gameState.strike = 0;
-  gameState.ball = 0;
+
 }
-
-
-
-
 
 init();
 
